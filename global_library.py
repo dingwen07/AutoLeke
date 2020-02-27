1e1f3186ead86919fb8bb07744ba9cb8293f98d0
赵佳成 <admin@g497813927.com>
2020-02-27 21:17:42 +0800

    feat: Support More type Detection

--- global_library.py
+++ global_library.py
@@ -1,24 +1,29 @@
-def input_check(prompt, input_type, acceptance):
+def input_check(prompt, input_type_example, acceptance, type_error_message="The type of the value you inputted is wrong!", not_in_range_message="The value you inputted is not in the range!"):
     while True:
         response = input(prompt)
-        if input_type == int:
+        input_type = type(input_type_example)
+        try:
+            input_type(response)
+        except ValueError:
+            print(type_error_message)
+        else:
             try:
-                int(response)
-            except ValueError:
-                print("The type of the value you inputted is wrong:\n\tYou need to input the type " + str(input_type))
+                acceptance
+            except NameError:
+                return response
             else:
                 try:
                     acceptance
                 except NameError:
                     return response
                 else:
-                    if __check_range(int(response), acceptance):
+                    if _check_range(input_type(response), acceptance, not_in_range_message):
                         return response
 
 
-def __check_range(response, acceptance):
+def _check_range(response, acceptance, not_in_range_message):
     if response in acceptance:
         return True
     else:
-        print("You have to enter in the range " + acceptance.__str__())
+        print(not_in_range_message)
         return False
